0427625d47cf48c001bee36d5ce14920be223e1d
Rut-Rocks-12 <54673663+Rut-Rocks-12@users.noreply.github.com>
2025-04-01 19:37:51 +0200

    Create creating-ppi-network.py

---
+++ creating-ppi-network.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from rdflib import Graph
+import networkx as nx
+# Load RDF file
+g = Graph()
+g.parse("huntington_string.rdf", format="turtle")
+
+# Extract protein interactions
+data = []
+for subj, _, obj in g:
+    protein1 = subj.split("/")[-1]  # Extract protein names
+    protein2 = obj.split("/")[-1]
+    data.append((protein1, protein2))
+
+# Save as CSV for Cytoscape
+df = pd.DataFrame(data, columns=["Protein A", "Protein B"])
+df.to_csv("huntington_string.csv", index=False)
+
+# Create NetworkX graph
+G = nx.Graph()
+for subj, _, obj in g:
+    protein1 = subj.split("/")[-1]
+    protein2 = obj.split("/")[-1]
+    G.add_edge(protein1, protein2)
+
+# Save as GraphML for Gephi
+nx.write_graphml(G, "huntington_string.graphml")
+
+# Draw the network
+plt.figure(figsize=(10, 7))
+nx.draw(G, with_labels=True, node_color="lightblue", edge_color="gray", font_size=10)
+plt.show()
+
+  
